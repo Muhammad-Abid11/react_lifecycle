d99fa37c52ba5fae07d8938a4529cf3ca98e79fe
Muhammad-Abid11 <abidkhan11845@gmail.com>
2022-05-26 03:13:47 +0500

    basic lifeCycles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,32 @@ class App extends React.Component {
 
   constructor() {
     super()
+    this.state = {
+      count: 0,
+    }
+    console.log(" life cycle ")
+    console.log(" 1.constructor, count=>", this.state.count)
+  }
+
+  static getDerivedStateFromProps() {
+    console.log(" 2.getDrivedStateFromProps")
+    // return null // return mendatory because it is static ,here 'this' is undefined
+    // console.log("Count==> ", this.state.count) //not work due to this
+    return {  //curly bracket
+      count: 5,
+    }
+  }
+
+  // shouldComponentUpdate() {
+
+  // }
+
+  componentDidMount() { //downside render its not work
+    console.log(" 4.component ")
   }
 
   render() {
+    console.log(" 3.render...")
     return (
       <div>
         <h1>Hello World</h1>
@@ -15,5 +38,15 @@ class App extends React.Component {
     )
   }
 }
+
+// getSnapshotBeforeUpdate(){
+
+// }
+
+// componentDidUpdate(){
+
+// }
+
+
 
 export default App;

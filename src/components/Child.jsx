97349a154cc0574d77d7e5a62e186f0e8d689df4
Muhammad-Abid11 <abidkhan11845@gmail.com>
2022-05-28 03:13:41 +0500

    console recommand child has its own initial state

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 export default class Child extends React.Component {
 
+    constructor() { // here initial state define just because console recommandation
+        super()
+        this.state = {
+            count: 0,
+            data: [],
+        }
+    }
+
     static getDerivedStateFromProps(props, state) {
         console.log("getDrivedStateFromProps in child", props)
         return null // return mendatory because it is static ,here 'this' is undefined
